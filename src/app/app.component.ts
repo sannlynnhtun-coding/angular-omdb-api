02d152c79c48f818019b81115904c952e2fe6f92
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2023-01-06 17:02:38 +0630

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,106 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild, ElementRef, OnInit} from '@angular/core';
+import { of } from "rxjs";
+import { debounceTime, map, distinctUntilChanged, filter} from "rxjs/operators";
+import { fromEvent } from 'rxjs';
+import { HttpClient, HttpParams } from "@angular/common/http";
+
+const APIKEY = "da979bab";
+
+const PARAMS = new HttpParams({
+  fromObject: {
+    action: "opensearch",
+    format: "json",
+    origin: "*"
+  }
+});
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-omdb-api';
+export class AppComponent implements OnInit{
+  @ViewChild('movieSearchInput', { static: true })
+  movieSearchInput!: ElementRef;
+  apiResponse: any;
+  isSearching: boolean;
+  movieDetails: any;
+  name:string='';
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument
+      .body.style.backgroundColor = 'purple';
+  }
+
+  constructor(
+    private elementRef: ElementRef,
+    private httpClient: HttpClient
+  ) {
+    this.isSearching = false;
+    this.apiResponse = [];
+    this.movieDetails = [];
+
+    console.log(this.movieSearchInput);
+  }
+
+  ngOnInit() {
+
+    console.log(this.movieSearchInput);
+
+this.isSearching = false;
+
+    fromEvent(this.movieSearchInput.nativeElement, 'keyup').pipe(
+
+      // get value
+      map((event: any) => {
+        return event.target.value;
+      })
+      // if character length greater then 2
+      , filter(res => res.length > 2)
+
+      // Time in milliseconds between key events
+      , debounceTime(1000)
+
+      // If previous query is diffent from current
+      , distinctUntilChanged()
+
+      // subscription for response
+    ).subscribe((text: string) => {
+
+      this.isSearching = true;
+
+      this.searchGetCall(text).subscribe((res) => {
+        console.log('res', res);
+        this.isSearching = false;
+        this.apiResponse = res;
+      }, (err) => {
+        this.isSearching = false;
+        console.log('error', err);
+      });
+
+    });
+  }
+
+  searchGetCall(term: string) {
+    if (term === '') {
+      return of([]);
+    }
+
+    return this.httpClient.get('http://www.omdbapi.com/?s=' + term + '&apikey=' + APIKEY, { params: PARAMS.set('search', term) });
+  }
+
+
+  isShowDiv = false;
+  getDetails(movie: any){
+    this.name= movie.Title;
+    this.isShowDiv = true;
+    this.httpClient.get('http://www.omdbapi.com/?i=' + movie.imdbID + '&apikey=' + APIKEY, { params: PARAMS.set('search', movie.imdbID) })
+      .subscribe(data=> {
+        //console.log('res', data);
+        this.movieDetails=data;
+      })
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatChipsModule} from '@angular/material/chips';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatChipsModule,
+
+    HttpClientModule,
+     NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
